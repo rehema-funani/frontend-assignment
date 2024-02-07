f72d8a51c52008503e580473dd9d4a9f577ded93
rehema-funani <rehemafunani@gmail.com>
2024-02-07 07:54:20 +0300

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var fullName = "Rehema Funani:"
+    println(fullName)
+    var age = "20"
+    println(age)
+    var phoneNumber = "0742530658"
+    println(phoneNumber)
+    var weight = "50"
+    println(weight)
+    var citizen = "kenyan"
+    println(citizen)
+}
